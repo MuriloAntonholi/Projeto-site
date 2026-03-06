6f366e4509912fff1ecd4075224f0919da937b86
MuriloAntonholi <144972588+MuriloAntonholi@users.noreply.github.com>
2026-03-06 13:13:06 -0300

    Add files via upload

---
+++ projeto site/projetoSite/js/products.js
@@ -0,0 +1,86 @@
+const products = [
+
+{
+id:1,
+name:"Galaxy S24",
+category:"celulares",
+price:4200,
+rating:9.0,
+
+pros:[
+"ótima câmera",
+"tela AMOLED excelente",
+"desempenho rápido"
+],
+
+cons:[
+"preço alto",
+"bateria poderia durar mais"
+],
+
+specs:{
+tela:"6.2 AMOLED",
+ram:"8GB",
+armazenamento:"256GB",
+bateria:"4000 mAh"
+},
+
+link:"https://amazon.com"
+},
+
+{
+id:2,
+name:"iPhone 15",
+category:"celulares",
+price:5200,
+rating:9.2,
+
+pros:[
+"câmera excelente",
+"sistema fluido",
+"muito rápido"
+],
+
+cons:[
+"caro",
+"poucas mudanças"
+],
+
+specs:{
+tela:"6.1 OLED",
+ram:"6GB",
+armazenamento:"128GB",
+bateria:"3877 mAh"
+},
+
+link:"https://amazon.com"
+},
+
+{
+id:3,
+name:"Dell XPS 13",
+category:"notebooks",
+price:8500,
+rating:9.1,
+
+pros:[
+"muito leve",
+"excelente tela",
+"ótimo desempenho"
+],
+
+cons:[
+"preço alto",
+"poucas portas"
+],
+
+specs:{
+processador:"Intel i7",
+ram:"16GB",
+ssd:"512GB"
+},
+
+link:"https://amazon.com"
+}
+
+]
